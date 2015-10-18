e733590bd8f9b3d13592c76c730fac7f7bc56ca8
jgb93 <jgb93@ougl-16-d.eplt.washington.edu>
2015-10-18 16:07:50 -0700

    Added code to implement tiles from MapBox

--- js/map.js
+++ js/map.js
@@ -1,8 +1,14 @@
 var drawMap = function() {
  	var map = L.map('mymap').setView([35.25, -98.88], 4);
-	var layer = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
- 	layer.addTo(map);
- 	getData();
+	var layer = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'jbossiere.cifu543lk1lhpudm0h9cfsnag',
+    accessToken: 'pk.eyJ1IjoiamJvc3NpZXJlIiwiYSI6ImNpZnU1NDR5ZzFsYjd0Z2x5MTI3Ym8zbmsifQ.JQrEEzRo9GxQYlygE4qaHg'
+  }).addTo(map);
+  /*L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+  }).addTo(map);*/
 }
 
 // Function for getting data
