c1c958816c536a9f2a08cfac31074d415f227cf7
jgb93 <jgb93@ougl-74-b.eplt.washington.edu>
2015-10-18 21:18:56 -0700

    No change

--- js/map.js
+++ js/map.js
@@ -12,9 +12,7 @@ var drawMap = function() {
       id: 'jbossiere.cifu543lk1lhpudm0h9cfsnag',
       accessToken: 'pk.eyJ1IjoiamJvc3NpZXJlIiwiYSI6ImNpZnU1NDR5ZzFsYjd0Z2x5MTI3Ym8zbmsifQ.JQrEEzRo9GxQYlygE4qaHg'
     }).addTo(map);
-    console.log('Testing drawMap');
     getData();
-    console.log('Testing getData');
 
   // Add the layer to your map
  
@@ -27,20 +25,14 @@ var drawMap = function() {
 var getData = function() {
 
   // Execute an AJAX request to get the data in data/response.js
-  $.getJSON('data/response.json').then(customBuild());
+  $.getJSON('../data/response.json').then(customBuild());
 
 
   // When your request is successful, call your customBuild function
 
 }
 
-// Loop through your data and add the appropriate layers and points
 var customBuild = function() {
-  //console.log('This is ' + array);
-  //for (i = 0; i < )
-  // Be sure to add each layer to the map
- 
-  // Once layers are on the map, add a leaflet controller that shows/hides layers
-  
+  console.log();
 }
-
+// Loop through your data and add the appropriate layers and points
