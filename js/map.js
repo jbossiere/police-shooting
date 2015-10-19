a8e589f6a836714f48c199f43379458c6932c1c3
Julian Bossiere <julianbossiere@gmail.com>
2015-10-19 01:29:21 -0700

    Completed challenge as much as possible. Added circles to map, added title, and additional info.

--- js/map.js
+++ js/map.js
@@ -1,38 +1,56 @@
+var unknownG = new L.layerGroup([]);
+var male = new L.layerGroup([]);
+var female = new L.layerGroup([]);
+var map;
+
 // Function to draw your map
 var drawMap = function() {
-
-  // Create map and set view
-  var map = L.map('mymap').setView([35.25, -98.88], 4);
- 
-
-  // Create a tile layer variable using the appropriate url
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-      maxZoom: 18,
-      id: 'jbossiere.cifu543lk1lhpudm0h9cfsnag',
-      accessToken: 'pk.eyJ1IjoiamJvc3NpZXJlIiwiYSI6ImNpZnU1NDR5ZzFsYjd0Z2x5MTI3Ym8zbmsifQ.JQrEEzRo9GxQYlygE4qaHg'
-    }).addTo(map);
-    getData();
-
-  // Add the layer to your map
- 
-
-  // Execute your function to get data
- 
+  map = L.map('mymap').setView([35.25, -98.88], 4);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+     maxZoom: 18,
+     id: 'jbossiere.cifu543lk1lhpudm0h9cfsnag',
+     accessToken: 'pk.eyJ1IjoiamJvc3NpZXJlIiwiYSI6ImNpZnU1NDR5ZzFsYjd0Z2x5MTI3Ym8zbmsifQ.JQrEEzRo9GxQYlygE4qaHg'
+  }).addTo(map);
+  getData(); 
 }
 
 // Function for getting data
 var getData = function() {
-
-  // Execute an AJAX request to get the data in data/response.js
-  $.getJSON('../data/response.json').then(customBuild());
-
-
-  // When your request is successful, call your customBuild function
-
+  $.getJSON('data/response.json').then(customBuild);
 }
 
-var customBuild = function() {
-  console.log();
+var customBuild = function(data) {
+  for (i = 0; i < data.length; i++) {
+
+    var lat = data[i]["lat"];
+    var lng = data[i]["lng"];
+
+    var name = data[i]["Victim Name"];
+
+    var armedOrNot = data[i]["Armed or Unarmed"];
+
+    var race = data[i]["Race"];
+
+    var state = data[i]["State"];
+
+    var gender = data[i]["Victim's Gender"];
+
+    var summary = data[i]["Summary"];
+
+    if (gender == 'Unknown') {
+      var circle = new L.circleMarker([lat, lng]);
+      unknownG[i] = data[i];
+      circle.addTo(unknownG).addTo(map);
+    } else if (gender == "Male") {
+      var circle = new L.circleMarker([lat, lng]);
+      male[i] = data[i];
+      circle.addTo(male).addTo(map);
+    } else {
+      var circle = new L.circleMarker([lat, lng]);
+      female[i] = data[i]
+      circle.addTo(female).addTo(map);
+    }
+    circle.bindPopup(summary);
+  }
 }
-// Loop through your data and add the appropriate layers and points
