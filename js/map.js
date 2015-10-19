4e6c107974adc712e7df106f90b6ae94d34f4992
jgb93 <jgb93@ougl-16-d.eplt.washington.edu>
2015-10-18 19:08:42 -0700

    Changes to map.js

--- js/map.js
+++ js/map.js
@@ -6,13 +6,15 @@ var drawMap = function() {
  
 
   // Create a tile layer variable using the appropriate url
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'jbossiere.cifu543lk1lhpudm0h9cfsnag',
-    accessToken: 'pk.eyJ1IjoiamJvc3NpZXJlIiwiYSI6ImNpZnU1NDR5ZzFsYjd0Z2x5MTI3Ym8zbmsifQ.JQrEEzRo9GxQYlygE4qaHg'
-}).addTo(map);
-
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+      maxZoom: 18,
+      id: 'jbossiere.cifu543lk1lhpudm0h9cfsnag',
+      accessToken: 'pk.eyJ1IjoiamJvc3NpZXJlIiwiYSI6ImNpZnU1NDR5ZzFsYjd0Z2x5MTI3Ym8zbmsifQ.JQrEEzRo9GxQYlygE4qaHg'
+    }).addTo(map);
+    console.log('Testing drawMap');
+    getData();
+    console.log('Testing getData');
 
   // Add the layer to your map
  
@@ -25,6 +27,7 @@ var drawMap = function() {
 var getData = function() {
 
   // Execute an AJAX request to get the data in data/response.js
+  $.getJSON('data/response.json').then(customBuild());
 
 
   // When your request is successful, call your customBuild function
@@ -33,8 +36,10 @@ var getData = function() {
 
 // Loop through your data and add the appropriate layers and points
 var customBuild = function() {
+  //console.log('This is ' + array);
+  //for (i = 0; i < )
   // Be sure to add each layer to the map
-
+ 
   // Once layers are on the map, add a leaflet controller that shows/hides layers
   
 }
